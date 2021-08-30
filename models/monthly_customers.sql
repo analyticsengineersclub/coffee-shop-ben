69a2eea401e17f9c1217dee4afa60a22c3460538
Ubuntu <ubuntu@ip-172-31-31-19.us-east-2.compute.internal>
2021-08-30 22:52:53 +0000

    Whoops, forgot ref function

--- models/monthly_customers.sql
+++ models/monthly_customers.sql
@@ -1,5 +1,5 @@
 select
   date_trunc(first_order_at, month) as first_order_month
   ,count(distinct customer_id) as n_customers
-from `dbt-tutorial-324521.dbt_bsorensen.customers`
+from {{ref('customers')}}
 group by 1
