1b90bb4ba47ca5e727ec00b79b9af489e00254bb
Ubuntu <ubuntu@ip-172-31-31-19.us-east-2.compute.internal>
2021-09-09 01:56:42 +0000

    set up first mart model and analysis

---
+++ models/marts/finance/order_item_revenues.sql
@@ -0,0 +1,30 @@
+with order_items as (
+    select * from {{ ref('stg_coffeeshop__order_items') }}
+),
+orders as (
+    select * from {{ ref('stg_coffeeshop__orders') }}
+),
+product_prices as (
+    select * from {{ ref('stg_coffeeshop__product_prices') }}
+),
+products as (
+    select * from {{ ref('stg_coffeeshop__products') }}
+)
+
+select 
+   orders.order_id 
+   ,products.product_id 
+   ,products.category as product_category
+   ,case when row_number() over (partition by orders.customer_id order by orders.created_at) > 1 then 'Returning'
+         when row_number() over (partition by orders.customer_id order by orders.created_at) = 1 then 'New'
+    end as customer_type
+   ,product_prices.price as price_at_order_creation
+   ,orders.created_at as order_created_at
+from orders 
+left join order_items  
+  on orders.order_id = order_items.order_id
+left join products on 
+  order_items.product_id = products.product_id
+left join product_prices 
+  on order_items.product_id = product_prices.product_id
+  and orders.created_at between product_prices.created_at and product_prices.ended_at
