368033f17215566f1d542b457dfd25a30a34a7f5
Ubuntu <ubuntu@ip-172-31-31-19.us-east-2.compute.internal>
2021-09-14 01:38:39 +0000

    fix web_sessionization joins

--- models/marts/digital/web_sessionization.sql
+++ models/marts/digital/web_sessionization.sql
@@ -1,9 +1,10 @@
 with pageviews as (
-    select * from {{ ref('user_stitching') }}
+    select * from `dbt-tutorial-324521.dbt_bsorensen.user_stitching`
 )
 ,minutes_between_sessions as (
 select 
-   visitor_id 
+   id
+   ,visitor_id
    ,session_recorded_at
    ,date_diff(
       session_recorded_at, 
@@ -11,21 +12,21 @@ select
       second
     ) / 60.0 as minutes_since_last_session
 from pageviews
-)
+)  
 ,new_sessions as (
 select 
-   visitor_id
+   id
+   ,visitor_id
    ,session_recorded_at
    ,case when coalesce(minutes_since_last_session > 30.0, true) then 1 else 0 end as is_new_session
 from minutes_between_sessions
-)
+) 
 ,final as (
 select 
-   visitor_id
-   ,session_recorded_at
+   id
    ,sum(is_new_session) over (partition by visitor_id order by session_recorded_at) as session_id
 from new_sessions
-)
+) 
 select 
    pageviews.id 
    ,concat(pageviews.visitor_id, '__s', final.session_id) as session_id 
@@ -37,6 +38,5 @@ select
    ,pageviews.original_visitor_id
 from pageviews 
 left join final on
-    pageviews.visitor_id = final.visitor_id and
-    pageviews.session_recorded_at = final.session_recorded_at
+    pageviews.id = final.id
 order by pageviews.session_recorded_at
